c0d6a1c77941d4d16aa964b135e12f079ff2707c
Mengjun <meggz899@gmail.com>
2025-11-27 11:32:13 +0100

    6

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Rezept-Plattform test5</div>;
+  return <div className="App">Rezept-Plattform test6</div>;
 }
 
 export default App;
